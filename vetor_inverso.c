f845a521c18a4a64822fe406104615f8e9356d64
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-11 15:55:52 -0300

    vetor_inverso.c

--- vetor_inverso.c
+++ vetor_inverso.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
 int main(void)
 {
 	setlocale(LC_ALL, "Portuguese");
 	int v[10], i;
 	for(i = 0; i < 10; i++){
-		printf("Digite o %dº valor: ", i + 1);
-		scanf("%d", &v[i]);
+	   printf("Digite o %dÂº valor: ", i + 1);
+	   scanf("%d", &v[i]);
 	}
-	printf("\n\nVetor\n\n");
+	printf("\n\nVetor\n");
 	for(i = 0; i < 10; i++){
-		printf("%d \n", v[i]);
+	   printf("%d \n", v[i]);
 	}
-	printf("\n\nVetor invertido\n\n");
+	printf("\n\nVetor Invertido\n");
 	for(i = 9; i >= 0; i--){
-			printf("%d \n", v[i]);
+	   printf("%d \n", v[i]);
 	}
-	
+	sytem("pause");
 	return 0;
 }
