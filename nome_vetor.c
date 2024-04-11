1712702e6762a015008d235c30dba87f22a3dfeb
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-11 16:45:08 -0300

    Update nome_vetor.c

--- nome_vetor.c
+++ nome_vetor.c
@@ -5,21 +5,12 @@ int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
     char nome[50], i = 1;
-    /*for(i = 0; i < 7; i++){
-        printf("Digite o %dº número: ", i + 1);
-        scanf("%d", &vet[i]);
-    }
-    printf("\nVetor\n");
-    for(i = 0; i < 7; i++){
-        printf("%d \n", vet[i]);
-    }
-	return 0;
-}*/
-		printf("Digite seu nome: ");
-		gets(nome);
-		while(nome[i] != '\0'){
-			i++;
-		}
-		printf("A quantidade de caracteres do seu nome é: %i", i);
-		return 0;
+  
+   printf("Digite seu nome: ");
+   gets(nome);
+   while(nome[i] != '\0'){
+	i++;
+   }
+   printf("A quantidade de caracteres do seu nome Ã©: %i", i);
+   return 0;
 }
