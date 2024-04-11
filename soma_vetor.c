859064485ff9edda864f413a5562616b882dbe0e
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-11 16:55:48 -0300

    Update soma_vetor.c

--- soma_vetor.c
+++ soma_vetor.c
@@ -6,13 +6,10 @@ int main(void)
     setlocale(LC_ALL, "Portuguese");
     int v[10], i, soma = 0;
     for(i = 0; i < 10; i++){
-        printf("Digite o %dº número: ", i + 1);
+        printf("Digite o %dÂº nÃºmero: ", i + 1);
         scanf("%d", &v[i]);
         soma += v[i];
     }
-    printf("A soma dos números do vertor é: %d", soma);
-    /*for(i = 0; i < 10; i++){
-        printf("%d \n", vet[i]);
-    }*/
-	return 0;
+    printf("A soma dos nÃºmeros do vertor Ã©: %d", soma);
+    return 0;
 }
