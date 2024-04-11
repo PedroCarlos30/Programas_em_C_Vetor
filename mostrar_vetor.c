158fe4384c43f6c9f8e573e4a3f1f9c0d03f9d36
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-11 16:33:12 -0300

    mostrar_vetor.c

--- mostrar_vetor.c
+++ mostrar_vetor.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <locale.h>
-
-void cv(int vet[]);
-
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
     int vet[7], i;
     for(i = 0; i < 7; i++){
-        printf("Digite o %dº número: ", i + 1);
+        printf("Digite o %dÂº nÃºmero: ", i + 1);
         scanf("%d", &vet[i]);
     }
     printf("\nVetor\n");
